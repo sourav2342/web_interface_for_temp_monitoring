34d6dcde0aa5d3938fec0be659a8ab2040bac979
traavrav <traavrav@gmail.com>
2023-02-19 17:45:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+var curr_temperature = 0;
+
+const express = require('express');
+const serv = express();
+const path = require('path');
+
+//serv.use(express.static(path.join(__dirname,'css')));
+
+/*serv.get("/", function(req, res){
+
+    res.sendFile(__dirname+"/index.html");
+});*/
+serv.use(express.static("html&css"));
+
+serv.listen(3000);
+
+serv.get("/data", function(req, res){
+
+    res.status(200).send(String(curr_temperature));
+
+});
+
+
+
+var http = require('http');
+  var server = http.createServer(function (req, res) {
+
+
+    if (req.method === 'POST' && req.url === '/post_data') {
+        let body = '';
+        req.on('data', function (data) {
+            body += data;
+        });
+        req.on('end', function () {
+
+           //console.log(body);
+
+           curr_temperature = Number(body);
+
+
+
+           //res.end('ok');
+        });
+    } else {
+        res.end('Not Found');
+    }
+});
+
+server.listen(80, function(){
+  console.log("Server is listening");
+});
